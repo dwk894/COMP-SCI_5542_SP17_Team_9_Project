16edf7b4a43fd82533d8fdaa414450f86776d66b
liuyunl777 <yunlongliu@yunlongs-mbp.attlocal.net>
2017-04-23 23:29:46 -0500

    twitter

---
+++ yunlong_app/out/artifacts/yunlong_app_war_exploded/js/exampleUsage.js
@@ -0,0 +1,27 @@
+
+var config = {
+  "id": '856341452833640448',
+  "domId": '',
+  "maxTweets": 50,
+  "enableLinks": true,
+  "showUser": true,
+  "showTime": true,
+  "dateFunction": '',
+  "showRetweet": false,
+  "customCallback": handleTweets,
+  "showInteraction": false
+};
+
+function handleTweets(tweets){
+    var x = tweets.length;
+    var n = 0;
+    var element = document.getElementById('example');
+    var html = '<ul>';
+    while(n < x) {
+      html += '<li>' + tweets[n] + '</li>';
+      n++;
+    }
+    html += '</ul>';
+    element.innerHTML = html;
+}
+twitterFetcher.fetch(config);
